4672f4c12dba4ff510c24a7e903a415526bfe667
Weslley Fornari <weslleyfornari@gmail.com>
2023-06-30 13:21:33 -0300

    Criado projeto Web services + H2

---
+++ src/main/java/com/weslleyfornari/webservicesh2/WebservicesH2Application.java
@@ -0,0 +1,13 @@
+package com.weslleyfornari.webservicesh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicesH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservicesH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/weslleyfornari/webservicesh2/WebservicesH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weslleyfornari.webservicesh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicesH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

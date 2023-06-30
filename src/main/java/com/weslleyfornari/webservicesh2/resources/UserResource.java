a35be0bbfda94431d2caa6e3e9c4bbcf6fbaef83
Weslley Fornari <weslleyfornari@gmail.com>
2023-06-30 16:27:23 -0300

    Criado Entidade USER e recursos, API REST

---
+++ src/main/java/com/weslleyfornari/webservicesh2/resources/UserResource.java
@@ -0,0 +1,22 @@
+package com.weslleyfornari.webservicesh2.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.weslleyfornari.webservicesh2.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	// -------------------------------------------------------------------------------------------------------------
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L,"Maria","maria@gmail.com", "99719-2389","1234456" );
+		return ResponseEntity.ok().body(u);
+	}
+	// -------------------------------------------------------------------------------------------------------------
+
+}
